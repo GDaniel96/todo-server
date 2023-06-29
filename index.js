7ee8b2cde912402c5a07be0fbb5df4d783a503bb
GDaniel96 <george.daniel1996@yahoo.com>
2023-06-29 16:52:00 +0300

    refactor with fs.promises and removed duplicated code

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3030;
-// const fs = require("fs").promises;
-const fs = require("fs");
-
-//EXTRACT DUPLICATED CODE IN A METHOD
-// REFACTOR CODE WITH FS PROMISES
+const fsp = require("fs").promises;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,92 +14,87 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-const readTodo = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(__dirname + "/" + "todos.json", "utf-8", (err, data) => {
-      resolve(data);
+const readTodo = async () => {
+  return await fsp
+    .readFile(__dirname + "/" + "todos.json", { encoding: "utf-8" })
+    .then((data) => {
+      return data;
     });
-  });
+};
+
+const writeDataOnDisk = async (newData) => {
+  const dataStringified = JSON.stringify(newData);
+
+  return await fsp
+    .writeFile(__dirname + "/" + "todos.json", dataStringified, {
+      encoding: "utf-8",
+    })
+    .then((data) => {
+      return data;
+    });
 };
 
 app.get("/", async (req, res) => {
   const data = await readTodo();
+
   res.send(data);
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
+  const data = await readTodo();
+
   const body = req.body;
+  const dataParse = JSON.parse(data);
+  const lastTodoIndex = dataParse.length - 1;
+  const lastTodo = dataParse[lastTodoIndex];
 
-  fs.readFile(__dirname + "/" + "todos.json", "utf-8", (err, data) => {
-    const dataParse = JSON.parse(data);
-    const lastTodoIndex = dataParse.length - 1;
-    const lastTodo = dataParse[lastTodoIndex];
+  const lastTodoId = lastTodoIndex < 0 ? 1 : lastTodo.id;
 
-    const lastTodoId = lastTodoIndex < 0 ? 1 : lastTodo.id;
+  const newData = [
+    ...dataParse,
+    {
+      ...body,
+      id: lastTodoId + 1,
+    },
+  ];
 
-    const newData = [
-      ...dataParse,
-      {
-        ...body,
-        id: lastTodoId + 1,
-      },
-    ];
-    fs.writeFile(
-      __dirname + "/" + "todos.json",
-      JSON.stringify(newData),
-      (err) => {
-        console.log(err);
-      }
-    );
-    res.send(body);
-  });
+  writeDataOnDisk(newData);
+  res.send(body);
 });
 
-app.delete("/:id", (req, res) => {
-  fs.readFile(__dirname + "/" + "todos.json", "utf-8", (err, data) => {
-    const dataParse = JSON.parse(data);
-    const todoId = parseInt(req.params.id);
-    const newData = dataParse.filter((todo) => {
-      if (todo.id === todoId) {
-        return false;
-      }
-      return true;
-    });
+app.delete("/:id", async (req, res) => {
+  const data = await readTodo();
 
-    fs.writeFile(
-      __dirname + "/" + "todos.json",
-      JSON.stringify(newData),
-      (err) => {
-        console.log(err);
-      }
-    );
-    res.send();
+  const dataParse = JSON.parse(data);
+  const todoId = parseInt(req.params.id);
+  const newData = dataParse.filter((todo) => {
+    if (todo.id === todoId) {
+      return false;
+    }
+    return true;
   });
+
+  writeDataOnDisk(newData);
+  res.send();
 });
 
-app.patch("/:id", (req, res) => {
-  fs.readFile(__dirname + "/" + "todos.json", "utf-8", (err, data) => {
-    const dataParse = JSON.parse(data);
-    const todoId = parseInt(req.params.id);
-    const newData = dataParse.map((todo) => {
-      if (todo.id === todoId) {
-        todo.isChecked = !todo.isChecked;
-        return todo;
-      }
+app.patch("/:id", async (req, res) => {
+  const data = await readTodo();
+
+  const dataParse = JSON.parse(data);
+  const todoId = parseInt(req.params.id);
+  const newData = dataParse.map((todo) => {
+    if (todo.id === todoId) {
+      todo.isChecked = !todo.isChecked;
       return todo;
-    });
-
-    fs.writeFile(
-      __dirname + "/" + "todos.json",
-      JSON.stringify(newData),
-      (err) => {
-        console.log(err);
-      }
-    );
-    res.send();
+    }
+    return todo;
   });
+
+  writeDataOnDisk(newData);
+  res.send();
 });
 
 app.listen(port, () => {
-  console.log(`Mizeria asta cica asculta la portu' ${port}`);
+  console.log(`Listening on port ${port}`);
 });
